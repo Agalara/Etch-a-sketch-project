0b0778dfb5982b7aae13900f607809f1979d8837
Alberto Garcia Lara <garcia.lara.alberto@gmail.com>
2023-03-07 16:19:39 +0100

    Added paint function. Chooses a random color

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,6 +1,16 @@
 
 let size =7;
 
+function getRandomIntInclusive(min,max){
+    min=Math.ceil(min);
+    max=Math.floor(max);
+    return Math.floor( Math.random()*(max - min + 1) + min);
+}
+
+
+
+
+
 function createCanvas (size){
 
     for(let i=0;i<size;i++){
@@ -19,5 +29,49 @@ function createCanvas (size){
     }       
 }
 
+function paint(){
+    const paintPixel = document.querySelectorAll('.pixel');
+    paintPixel.forEach((pixel) =>{
+        pixel.addEventListener('mouseover',function (e) {
+    
+            e.target.style.background = `rgb(${getRandomIntInclusive(0,255)},${getRandomIntInclusive(0,255)},${getRandomIntInclusive(0,255)})`;
+            console.log(e.target);
+        })
+    }) 
+}
+
+
+
 
 createCanvas(size);
+paint();
+
+
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e);
+// });
+
+// btn.addEventListener('click', function (e) {
+//     console.log(e.target);
+// });
+
+//  btn.addEventListener('click', function (e) {
+//      e.target.style.background = 'blue';
+//  });
+
+
+// // buttons is a node list. It looks
+// // and acts much like an array.
+// const buttons = document.querySelectorAll('button');
+
+// // we use the .forEach method to 
+// //iterate through each button
+// buttons.forEach((button) => {
+
+//   // and for each one we add a 'click'
+//   // listener
+//   button.addEventListener('click', () => {
+//     alert(button.id);
+//   });
+// });
